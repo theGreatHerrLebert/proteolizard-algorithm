8aad8035979b974d1e8edf855865b2eb6ff1be59
Tim Maier (Ubuntu Desktop) <tmaier01@students.uni-mainz.de>
2023-02-21 11:32:26 +0100

    updated tests for isotope module

--- python/test/test_isotopes.py
+++ python/test/test_isotopes.py
@@ -1,10 +1,10 @@
 import pytest
 import numpy as np
-from scipy.stats import norm
 from collections import namedtuple
 
 from proteolizarddata.data import MzSpectrum
-from proteolizardalgo.isotopes import factorial, lam, weight, normal_pdf, iso, AveragineGenerator, MASS_NEUTRON
+from proteolizardalgo.isotopes import factorial, lam, weight, iso, AveragineGenerator, MASS_NEUTRON
+from proteolizardalgo.utility import normal_pdf
 
 Peptide = namedtuple("Peptide", ["mass", "charge"])
 
@@ -22,7 +22,8 @@ def test_lam(peptide):
     # tested against status 16 feb 23
     assert np.isclose(lam(peptide.mass),0.000594*peptide.mass+(-0.03091)), "Calculated averagine lambda is unexpected"
 
-def test_weight(peptide):
+@pytest.mark.parametrize("normalize",[True,False])
+def test_weight(peptide,normalize):
     # tested against status 16 feb 23
     mass = peptide.mass
     number_peaks = 5
@@ -32,20 +33,15 @@ def test_weight(peptide):
         iso_factorials[i] = factorial(peak)
     averagine_lam = lam(peptide.mass)
     expected_weights = np.exp(-averagine_lam)*np.power(averagine_lam,peaks) / iso_factorials
-    calculated_weights = weight(mass, peaks)
-    assert np.allclose(expected_weights,calculated_weights), "Calculation of isotopic peak weights is invalid"
-
-def test_normal_pdf(peptide):
-    # tested against scipy
-    mu = peptide.mass
-    sigma = 0.01
-    x = np.array([mu-sigma,mu,mu+sigma])
-    calculated_ps = normal_pdf(x,mu,sigma)
-    expected_ps = norm.pdf(x,loc=mu,scale=sigma)
-    assert np.allclose(calculated_ps,expected_ps), "Calculation of normal probabilities is not corresponding to scipy implementation"
+    expected_weights_norm = expected_weights/expected_weights.sum()
+    calculated_weights = weight(mass, peaks,normalize)
+    if normalize:
+        assert np.allclose(expected_weights_norm,calculated_weights), "Calculation of normalized isotopic peak weights is invalid"
+    else:
+        assert np.allclose(expected_weights,calculated_weights), "Calculation of isotopic peak weights is invalid"
 
 def test_iso(peptide):
-    # tested against status 16 feb 23
+    # tested against status 21 feb 23
     mass = peptide.mass
     charge = peptide.charge
     iso_distance = MASS_NEUTRON/charge
@@ -53,13 +49,14 @@ def test_iso(peptide):
     amp = 1
     K = 7
     x = np.array([mass,mass+iso_distance,mass+2*iso_distance])
-    calculated_intensities = iso(x,mass,charge,sigma,amp,K)
+    # this step size is way too big, just for testing purpose
+    calculated_intensities = iso(x,mass,charge,sigma,amp,K,step_size=iso_distance,add_detection_noise=False)
 
     # expected
     k = np.arange(K)
     means = ((mass + MASS_NEUTRON * k) / charge).reshape((1,-1))
     weights = weight(mass,k).reshape((1,-1))
-    intensities = np.sum(weights*normal_pdf(x.reshape((-1,1)), means, sigma), axis= 1)
+    intensities = np.sum(weights*normal_pdf(x.reshape((-1,1)), means, sigma), axis= 1)*iso_distance
     expected_intensities =  intensities * amp
 
     np.allclose(calculated_intensities, expected_intensities)
